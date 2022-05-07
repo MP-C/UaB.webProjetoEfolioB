27bc86bf0b718eefb132ff83f4d7f0042a4d164f
Mario P Carvalho <mp.crcarvalho@gmail.com>
2022-05-07 02:31:29 +0200

    funções js incluidas e  ok

--- javascript.js
+++ javascript.js
@@ -1,3 +1,42 @@
+/* JavaScript*/
+/* Para funções que escondem o HTML*/
+function htmlShowHome(){
+    var x = document.getElementById("htmlHome");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function htmlShowVisit(){
+    var x = document.getElementById("htmlVisit");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function htmlShowContact(){
+    var x = document.getElementById("htmlContact");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function htmlShowMail(){
+    var x = document.getElementById("htmlMail");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+/* Para enviar informação de mail e redirecionar  */
 function sendForm(){
     var d = new Date();
     alert("HELLO!\nToday " + d.toDateString() + " you will send a formulaire asking for something. We will keep yout contact for further instructions.")
@@ -7,22 +46,18 @@ function sendForm(){
     //window.location.reload(true);
 }
 
+/* para voltar à pagina anterior */
 function retur(){
     window.history.back();
 }
 
+/*Para reenviar para o link do repositório */
 function reposLink(){
     window.location.assign("https://github.com/MP-C/WebProjetoEfolioA");
     //window.location.assign("https://github.com/MP-C/WebProjetoEfolioB/releases/tag/v1.0");
 }
 
-
-function darkModeButton() {
-    var element = document.section;
-    console.log("Dark mode element");
-    element.buttonDark.toggle("dark-mode");
-}
-
+/*para fazer download do projeto caso necesario */
 function DownloadWebFiles() {
     function download(fileUrl, fileName){
     var a = document.createElement("a");
@@ -33,6 +68,7 @@ function DownloadWebFiles() {
     download("https://drive.google.com/drive/folders/1bWDt6dVxIbo3o1qKUN2Gc7TCcL6CdagH?usp=sharing", "EFolioB_MarioCarvalho_web");
 }
 
+/*Função quer permite activar a localização, que é uma funcionalidade, mas fica a faltar por enquanto integrar com o mapa */
 var x = document.getElementById("location");
 function getLocation() {
     if (navigator.geolocation) {
